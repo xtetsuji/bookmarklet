5c3ae72389d78c289ca32b09ad01dac43a8c4663
OGATA Tetsuji <tetsuji.ogata@gmail.com>
2013-07-08 18:06:37 +0900

    gen_gcalep_link.js: Fix bugs.

--- parts/gen_gcalep_link.js
+++ parts/gen_gcalep_link.js
@@ -1,14 +1,17 @@
+var DEBUG = 0;
+
 var pad = function(digit) {
     if(digit.toString().length==1) digit="0"+digit.toString();
     return digit;
-},
-to_utcdigit = function(jst) { // String jst
+};
+
+var to_utcdigit = function(jst) { // String jst
     var date, incl_date=false;
     if(m=jst.match(/ (2[4-9]):([0-9][0-9])/)) {
         incl_date = true;
         jst = jst.replace(/ 2[4-9]:[0-9][0-9]/, " "+pad(parseInt(m[1])-24)+":"+m[2]);
     }
-    //console.log("jst="+jst);
+    if(DEBUG){console.log("jst="+jst);}
     date = new Date(jst); // valid JST Date string
     if(incl_date){
         date.setDate(date.getDate()+1); // 末日バグ等は無いはず
@@ -16,10 +19,12 @@ to_utcdigit = function(jst) { // String jst
     //console.log("dated_jst="+date.toString());
     //console.log("incl_date is " + (incl_date?"true":"false"));
     return(date.getUTCFullYear().toString()+pad(date.getUTCMonth()+1)+pad(date.getUTCDate())+"T"+pad(date.getUTCHours())+pad(date.getUTCMinutes())+pad(date.getUTCSeconds())+"Z");
-},
-gen_gcalep_link = function(arg) {
+};
+
+var gen_gcalep_link = function(arg) {
     // args: text, url, desc, location,
-    //       from, to (valid Date object)
+    //       from, to (valid string as Date object constructor 1st argument)
+    if(DEBUG){console.log("gen_gcalep_lin argument", arg);}
     var gparam = {};
     var gparam_array = [];
     var gparam_keys = ["action", "text", "dates", "sprop", "details", "location"];
@@ -30,9 +35,9 @@ gen_gcalep_link = function(arg) {
     gparam["action"]   = arg["action"] || "TEMPLATE";
     gparam["text"]     = arg["text"];
     gparam["dates"]    = dates;
-    gparam["sprop"]    = arg["url"].replace(/^http:/,"website:");
-    gparam["details"]  = arg["url"]+"\n"+(arg["desc"]||"");
-    gparam["location"] = arg["location"];
+    gparam["sprop"]    = arg["url"]?arg["url"].replace(/^http:/,"website:"):"";
+    gparam["details"]  = (arg["url"]?arg["url"]+"\n":"")+(arg["desc"]||"");
+    gparam["location"] = arg["location"] || "";
     gparam_keys.forEach(
         function(key){
             gparam_array.push(key+"="+encodeURIComponent(gparam[key]));
